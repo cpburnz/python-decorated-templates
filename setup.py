501c8b6f44eddc27eea08b4d43f4b2ff85341e0d
Caleb Burns <cpburnz@gmail.com>
2012-08-23 15:42:43 -0400

    fix

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import warnings
 from distutils.core import setup
 
 if sys.version_info[:2] != (2, 7):
-	warnings.warn("Only Python 2.7 has been tested, not %s.%s" % sys.version_info[:2]
+	warnings.warn("Only Python 2.7 has been tested, not %s.%s" % sys.version_info[:2])
 
-import .pdt
+import pdt
 
 setup(
-	name="pdt"
+	name="pdt",
 	version=pdt.__version__,
 	author="Caleb P. Burns",
 	author_email="cpburnz@gmail.com",
@@ -21,7 +21,7 @@ setup(
 		"Intended Audience :: Developers",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
-		"Programming Language :: Python"
+		"Programming Language :: Python",
 		"Programming Language :: Python :: 2.7",
 		"Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
 		"Topic :: Software Development :: Libraries :: Python Modules"
